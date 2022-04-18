08d8ce0365149bf10a6e6f1e09a7a1e55813eb95
erhshih <jerry0412809@gmail.com>
2022-04-18 14:10:36 +0800

    deploy

---
+++ js/about-legacy.425b111e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_lottery"]=self["webpackChunkvue_lottery"]||[]).push([[443],{5830:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},l=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],r=n(1001),s={},a=(0,r.Z)(s,u,l,!1,null,null,null),c=a.exports}}]);
+//# sourceMappingURL=about-legacy.425b111e.js.map
